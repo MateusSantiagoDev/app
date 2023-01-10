ad6cb1da4121b9188b89c0f5211eb481f96ec31e
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-01-10 13:21:14 -0300

    feat(app): updated project and  adicionad react router dom

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,7 +1,23 @@
 import "./Home.css";
+import logo from "../assets/images/logo.png";
+import { HiOutlineMail } from 'react-icons/hi';
+import { RiWhatsappFill } from 'react-icons/ri';
 
-export function Home(){
-    return(
-        <div>Ola</div>
-    )
+export function Home() {
+  return (
+    <div className="container">
+      <div className="logo">
+        <img src={logo} alt="logo" />
+        <div className="contato">
+         {/*  <a href="/" target="_blank"><HiOutlineMail/></a>
+          <a href="/" target="_blank"><RiWhatsappFill/></a> */}
+          <button>Caça Vazamento</button>
+          <button>Limpeza de Caixa d'agua</button>
+          <button>Desentupimento</button>
+          <button>Desalojamento de Pombo</button>
+        </div>
+      </div>
+      <div className="home"></div>
+    </div>
+  );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,15 @@
 import "./assets/Style.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./Page/Home";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <Home/>
-    </React.StrictMode>
-)
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/> 
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
