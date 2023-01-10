7b3cda9de67aff77cdd50aa5b19e458bff4b60fc
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-01-10 11:25:19 -0300

    feat(app): starting project implementation

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import "./assets/Style.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Home } from "./Page/Home";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <Home/>
+    </React.StrictMode>
+)
